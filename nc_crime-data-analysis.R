8ebb935fdc357dccd3b469a599e8a650c7455f1d
vdmitriyev <dmitriyev.viktor@gmail.com>
2015-07-28 16:48:52 +0200

    Update course materials

--- nc_crime-data-analysis.R
+++ nc_crime-data-analysis.R
@@ -7,6 +7,7 @@ class(crime)
 # defininf the model
 model <- lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban
 
+
 # Pooled model
 panel.ols <- plm(model, data=crime , model="pooling")
 summary(panel.ols)
@@ -15,16 +16,23 @@ summary(panel.ols)
 panel.be<-plm(model, data=crime , model="between")
 summary(panel.be)  #Baltagi column 1 table 7.1 page 133
 
+# with dummy variables
+modelSecond <- lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban + d82 + d83 + d84 + d85 + d86 + d87
+  
 #Fixed effect with time dummies
-panel.fe<-plm(modelt, data=crime , model="within")
+panel.fe <- plm(modelSecond, data=crime , model="within")
 summary(panel.fe)   #Baltagi column 2 table 7.1 page 133
 summary(fixef(panel.fe))
 
-iv.fe<-plm(lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban+d82+d83+d84+d85+d86+d87|.-lprbarr -lpolpc + ltaxpc + lmix, data=crime, model="within")
-summary(iv.fe)  #FE2SLS Baltagi Table 7.1 p.133 column 3
+#
+# Change the variable with additional "instrumental" influence
+#
 
-iv.be<-plm(lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban|.-lprbarr -lpolpc + ltaxpc + lmix, data=crime, model="between")
-summary(iv.be)  #BE2SLS Baltagi Table 7.1 p.133 column 4
+modelWithInstrumentsBasic <- plm(lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban+d82+d83+d84+d85+d86+d87|.-lprbarr -lpolpc + ltaxpc + lmix, data=crime, model="within")
+summary(modelWithInstrumentsBasic)  #FE2SLS Baltagi Table 7.1 p.133 column 3
 
-iv.re<-plm(lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban+d82+d83+d84+d85+d86+d87|.-lprbarr -lpolpc + ltaxpc + lmix, data=crime, model="random", inst.method="baltagi")
-summary(iv.re) #BE2SLS Baltagi Table 7.1 p.133 column 5
+modelWithInstrumentsBE <- plm(lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban|.-lprbarr -lpolpc + ltaxpc + lmix, data=crime, model="between")
+summary(modelWithInstrumentsBE)  #BE2SLS Baltagi Table 7.1 p.133 column 4
+
+modelWithInstrumentsRandomVariables <- plm(lcrmrte~lprbarr+lprbconv+lprbpris+lavgsen+lpolpc+ldensity+lwcon+lwtuc+lwtrd+lwfir+lwser+lwmfg+lwfed+lwsta+lwloc+lpctymle+lpctmin+west+central+urban+d82+d83+d84+d85+d86+d87|.-lprbarr -lpolpc + ltaxpc + lmix, data=crime, model="random", inst.method="baltagi")
+summary(modelWithInstrumentsRandomVariables) #BE2SLS Baltagi Table 7.1 p.133 column 5
